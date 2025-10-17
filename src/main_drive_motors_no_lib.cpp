16b22ca96acf6b1dd88ce06e8132b74e36f3e04b
Dede Dodu <sebnat94@hotmail.com>
2025-10-17 23:39:18 +0200

    drive_motors: Test driving the motors
    Two new programs introduced for trying to drive the motors through the
    ESCs. The first program is using the Servo library which is meant for
    some basic testing. The second program is more advanced since it
    controls the arduino ports directly. This gives much higher control in
    driving the PWM that is driven through every ESC, allowing for a higher
    frequency than the default 50Hz (20ms period).
    
    Signed-off-by: Dede Dodu <sebnat94@hotmail.com>

---
+++ src/main_drive_motors_no_lib.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+
+/*
+#define MOTOR1_PIN 5
+#define MOTOR2_PIN 6
+#define MOTOR3_PIN 9
+#define MOTOR4_PIN 10
+
+void setup() {
+    pinMode(MOTOR1_PIN, OUTPUT);
+    pinMode(MOTOR2_PIN, OUTPUT);
+    pinMode(MOTOR3_PIN, OUTPUT);
+    pinMode(MOTOR4_PIN, OUTPUT);
+}
+
+
+void loop() {
+    digitalWrite(MOTOR1_PIN, HIGH);
+    digitalWrite(MOTOR2_PIN, HIGH);
+    digitalWrite(MOTOR3_PIN, HIGH);
+    digitalWrite(MOTOR4_PIN, HIGH);
+
+    digitalWrite(MOTOR1_PIN, LOW);
+    digitalWrite(MOTOR2_PIN, LOW);
+    digitalWrite(MOTOR3_PIN, LOW);
+    digitalWrite(MOTOR4_PIN, LOW);
+}
+*/
+
+void setup() { DDRB |= 0b00000100; }
+
+void loop() {
+  PORTB |= 0b00000100;
+  PORTB &= 0b11111011;
+}
